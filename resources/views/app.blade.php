1a1c28cca26b8dc563e23e006f5f60bad5ca0f4e
Hasbi1605 <terry.delvon0805@gmail.com>
2026-05-30 15:46:06 +0700

    Update public preview title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <title>ISTURA - Istana Untuk Rakyat Yogyakarta</title>
+    <title>ISTURA - Istana Untuk Rakyat</title>
+    <meta property="og:title" content="ISTURA - Istana Untuk Rakyat" />
+    <meta name="twitter:title" content="ISTURA - Istana Untuk Rakyat" />
     <link rel="icon" type="image/png" href="/assets/gedung-agung-gold.png" />
     <script>
         window.__ISTURA_CONFIG__ = @json(['publicAppUrl' => config('app.url')]);
